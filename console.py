23157d69e735de1a7b70913e3e5479b3026525b8
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-20 22:36:15 +0500

    add rest api methods put post get delete

--- console.py
+++ console.py
@@ -1,4 +1,4 @@
 import requests
 
-res = requests.get("127.0.0.1:3000/api/main")
+res = requests.get("127.0.0.1:3000/api/courses/2")
 print(res.json())
--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api()
 
+
+courses = {1: {"name": "Java", "videos" : 10},
+           2: {"name": "Kotlin", "videos" : 15},
+           3: {"name": "Go", "videos" : 20}}
+
+
+
+class Main(Resource):
+
+    def get(self, cources_id):
+
+        if cources_id != 0:
+            courses[cources_id]
+        else:
+            return courses
+
+    def delete(self, cources_id):
+        del courses[cources_id]
+        return courses
+
+    def post(self, couces_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", type=str)
+        parser.add_argument("videos", type=int)
+        courses[couces_id] = parser.parse_args()
+
+    def put(self, cources_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", type=str)
+        parser.add_argument("videos", type=int)
+        courses[cources_id] = parser.parse_args()
+
+
+
+api.add_resource(Main, "/api/courses/<int:cources_id>")
+api.init_app(app)
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000, host="127.0.0.1")
