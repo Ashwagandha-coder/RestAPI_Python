62cfa4a5b3b61218a9465030980bbd2b7c302b47
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-20 21:09:41 +0500

    add run project

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from flask import Flask
 from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000, host="127.0.0.1")
